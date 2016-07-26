c812bcbdce1ad86824d006c91086bde4d554954b
Vladimir Kudinov <frux@yandex-team.ru>
2016-07-26 17:44:44 +0500

    Init

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function Graphon(endpoint, params) {
 		var inlineQuery = encodeURIComponent(query);
 		return `${endpoint}?query=${inlineQuery}`;
 	};
+
+	this._cacheStorage = {};
 }
 
 /**
@@ -20,6 +22,7 @@ function Graphon(endpoint, params) {
  * @param {string} url – Url to fetch
  * @param {object} options – Request options
  * @param {'omit'|'same-origin'|'include'|undefined} oprions.credential – Should send cookies
+ * @returns {Promise}
  */
 Graphon.prototype._fetch = function (url, options) {
 	options = options || {};
@@ -44,49 +47,115 @@ Graphon.prototype._fetch = function (url, options) {
  * Fetches GraphQL Query
  * @param {string} url – Url to fetch
  * @param {object} options – Request options
- * @param {string} options.method – Request method. Default 'GET'.
- * @param {'omit'|'same-origin'|'include'|undefined} oprions.credential – Should send cookies. Default 'omit'.
+ * @param {number} cache – Time to live cache in ms
+ * @param {object} options.fetch – Fetch options
+ * @param {string} options.fetch.method – Request method. Default 'GET'.
+ * @param {'omit'|'same-origin'|'include'|undefined} oprions.fetch.credential – Should send cookies. Default 'omit'.
+ * @returns {Promise}
  */
 Graphon.prototype.query = function (query, options) {
 	options = options || {};
+	const queryURL = this._getQuery(query);
 	const fetchOptions = options.fetch || {};
 	fetchOptions.method = fetchOptions.method || 'GET';
 	fetchOptions.credential = fetchOptions.credential || 'omit';
+
+	if (options.cache && this._cacheStorage[queryURL] && !this._cacheStorage[queryURL].isOutdated()) {
+		return Promise.resolve(this._cacheStorage[queryURL].getData());
+	}
 
 	if (typeof query !== 'string') {
 		throw new Error('Query must be a string');
 	}
 
-	return this._fetch(this._getQuery(query), fetchOptions);
+	return this._fetch(queryURL, fetchOptions)
+		.then(function (data) {
+			if (options.cache) {
+				if (this._cacheStorage[queryURL]) {
+					this._cacheStorage[queryURL].update(data);
+				} else {
+					this._cacheStorage[queryURL] = new GraphonCache(data, options.cache);
+				}
+			}
+			return data;
+		});
 };
 
 /**
  * Fetches GraphQL Mutation
  * @param {string} url – Url to fetch
- * @param {string} options.method – Request method. Default 'POST'.
- * @param {'omit'|'same-origin'|'include'|undefined} oprions.credential – Should send cookies. Default 'omit'.
+ * @param {object} options – Request options
+ * @param {number} cache – Time to live cache in ms
+ * @param {object} options.fetch – Fetch options
+ * @param {string} options.fetch.method – Request method. Default 'POST'.
+ * @param {'omit'|'same-origin'|'include'|undefined} oprions.fetch.credential – Should send cookies. Default 'omit'.
+ * @returns {Promise}
  */
 Graphon.prototype.mutation = function (query, options) {
 	options = options || {};
+	const queryURL = this._getQuery(query);
 	const fetchOptions = options.fetch || {};
 	fetchOptions.method = fetchOptions.method || 'POST';
 	fetchOptions.credential = fetchOptions.credential || 'omit';
+
+	if (options.cache && this._cacheStorage[queryURL] && !this._cacheStorage[queryURL].isOutdated()) {
+		return Promise.resolve(this._cacheStorage[queryURL].getData());
+	}
 
 	if (typeof query !== 'string') {
 		throw new Error('Query must be a string');
 	}
 
-	return this._fetch(this._getQuery(query), fetchOptions);
+	return this._fetch(queryURL, fetchOptions)
+		.then(function (data) {
+			if (options.cache) {
+				if (this._cacheStorage[queryURL]) {
+					this._cacheStorage[queryURL].update(data);
+				} else {
+					this._cacheStorage[queryURL] = new GraphonCache(data, options.cache);
+				}
+			}
+			return data;
+		});
 };
 
+/**
+ * Graphon request cache class
+ * @param {*} data – Any data to cache
+ * @param {number} ttl – Time to live in ms
+ * @constructor
+ */
 function GraphonCache(data, ttl) {
-	this._data = data;
-	this._timestamp = Number(new Date());
 	this._ttl = ttl || 0;
+	this._data = data;
+	this._updateTime();
 }
 
-GraphonCache.prototype.isOutdated = function () {
+/**
+ * Update cached data
+ * @param {*} newData – New data
+ * @returns {GraphonCache}
+ */
+GraphonCache.prototype.update = function (newData) {
+	this._data = newData;
+	this._timestamp = Number(new Date());
+	return this;
+};
 
+/**
+ * Returns cached data
+ * @returns {*}
+ */
+GraphonCache.prototype.getData = function () {
+	return this._data;
+};
+
+/**
+ * Check if cache is outdated
+ * @returns {boolean}
+ */
+GraphonCache.prototype.isOutdated = function () {
+	return (Number(new Date()) - this._timestamp) > this._ttl;
 };
 
 /**
